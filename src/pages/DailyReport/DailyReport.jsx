59ee7b409b970d0070780d348a475a6fe6890f5f
iurie <nabiiru7@gmail.com>
2025-12-13 10:06:43 +0700

    update

--- src/pages/DailyReport/DailyReport.jsx
+++
@@ -1,3 +0,0 @@
-export default function DailyReport() {
-  return <h2 className="text-xl font-bold">Daily Report Here!</h2>;
-}
--- src/pages/Team/Team.jsx
+++
@@ -1,3 +0,0 @@
-export default function Team() {
-  return <h2 className="text-xl font-bold">Team Here!</h2>;
-}
